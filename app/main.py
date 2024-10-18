4a79037478519755a74fc0b5ae108890a30fc3d2
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-19 02:19:59 +0600

    clear project

--- app/main.py
+++ app/main.py
@@ -3,9 +3,6 @@ from app.api import auth, users
 
 app = FastAPI()
 
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
-app.include_router(users.router, prefix="/users", tags=["users"])
-
 
 @app.get("/")
 def read_root():

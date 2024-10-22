a5768a92a7c43b56209c74041ada7713e88ddfe9
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-22 14:08:31 +0600

    added cors middle

--- server/app/main.py
+++ server/app/main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import auth, post, photo
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(post.router, prefix="/post", tags=["post"])
 app.include_router(photo.router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI Auth Backend"}
